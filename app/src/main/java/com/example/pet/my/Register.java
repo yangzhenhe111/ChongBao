4d9f0e5e28ecaba92b290c62abad39d25661e7ae
yangzhenhe111 <3258274495@qq.com>
2020-12-02 16:39:29 +0800

    gai

--- app/src/main/java/com/example/pet/my/Register.java
+++ app/src/main/java/com/example/pet/my/Register.java
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
+import android.opengl.ETC1;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.pet.R;
@@ -14,6 +17,11 @@ import com.example.pet.R;
 public class Register extends AppCompatActivity {
 private Toolbar toolbar;
 private TextView registerAgreement;
+private TextView tvLogin;
+private EditText etPhone;
+private CheckBox cbXieYi;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,13 +30,8 @@ private TextView registerAgreement;
     }
 
     private void setView() {
-        toolbar = findViewById(R.id.register_toolbar);
-        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Register.this.finish();
-            }
-        });
+        etPhone = findViewById(R.id.et_phone);
+        cbXieYi = findViewById(R.id.cb_xieyi);
 
     }
 
@@ -38,6 +41,23 @@ private TextView registerAgreement;
                 Intent intent = new Intent(this,Agreement.class);
                 startActivity(intent);
                 break;
+            case R.id.tv_login:
+                Register.this.finish();
+                break;
+            case R.id.register_toolbar:
+                Register.this.finish();
+                break;
+            case R.id.btn_next1:
+                String str = etPhone.getText().toString();
+                if(str.length() == 11 && cbXieYi.isChecked() ){
+                    Intent intent1 = new Intent(this,SetPassword.class);
+                    intent1.putExtra("phone",str);
+                    startActivity(intent1);
+                }else {
+                    //提示位数错误
+                }
+
+
         }
     }
 }
--- app/src/main/java/com/example/pet/other/Cache.java
+++ app/src/main/java/com/example/pet/other/Cache.java
@@ -1,4 +1,8 @@
 package com.example.pet.other;
 
 public class Cache {
+    /**
+     * 存储一些全局信息
+     */
+
 }
