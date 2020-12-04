bc7e2c20d2e8f72c1267a50f679a77b943787cf5
yangzhenhe111 <3258274495@qq.com>
2020-12-04 10:00:35 +0800

    gai

--- app/src/main/java/com/example/pet/other/entity/User.java
+++ app/src/main/java/com/example/pet/other/entity/User.java
@@ -1,15 +1,53 @@
 package com.example.pet.other.entity;
 
 public class User {
-    private int userId;
-    private String userName;
-    private String accountNumber;
-    private String accountAmount;
-    private String picturePath;
-    private String userPassword;
-    private String userPhone;
-    private String userEmail;
-    private String userAutograph;
+    private int userId;//用户ID
+    private String userName;//用户姓名
+    private String accountNumber;//账号
+    private double accountAmount;//账户金额
+    private String picturePath;//图片路径
+    private String userPassword;//用户密码
+    private String userPhone;//用户手机号
+    private String userEmail;//用户邮箱
+    private String userAutograph;//个性签名
+
+    public User() {
+    }
+
+    public User(int userId, String userName, String accountNumber, double accountAmount, String picturePath, String userPassword, String userPhone, String userEmail, String userAutograph) {
+        this.userId = userId;
+        this.userName = userName;
+        this.accountNumber = accountNumber;
+        this.accountAmount = accountAmount;
+        this.picturePath = picturePath;
+        this.userPassword = userPassword;
+        this.userPhone = userPhone;
+        this.userEmail = userEmail;
+        this.userAutograph = userAutograph;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                ", userName='" + userName + '\'' +
+                ", accountNumber='" + accountNumber + '\'' +
+                ", accountAmount=" + accountAmount +
+                ", picturePath='" + picturePath + '\'' +
+                ", userPassword='" + userPassword + '\'' +
+                ", userPhone='" + userPhone + '\'' +
+                ", userEmail='" + userEmail + '\'' +
+                ", userAutograph='" + userAutograph + '\'' +
+                '}';
+    }
+
+    public double getAccountAmount() {
+        return accountAmount;
+    }
+
+    public void setAccountAmount(double accountAmount) {
+        this.accountAmount = accountAmount;
+    }
 
     public int getUserId() {
         return userId;
@@ -33,14 +71,6 @@ public class User {
 
     public void setAccountNumber(String accountNumber) {
         this.accountNumber = accountNumber;
-    }
-
-    public String getAccountAmount() {
-        return accountAmount;
-    }
-
-    public void setAccountAmount(String accountAmount) {
-        this.accountAmount = accountAmount;
     }
 
     public String getPicturePath() {
