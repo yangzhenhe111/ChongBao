8d4a80e4bcfa75e860e19dc540368d5a6146a9a9
maomuye <1484771988@qq.com>
2020-12-09 15:25:38 +0800

    first

--- app/src/main/java/com/example/pet/my/order/MyOrderActivity.java
+++ app/src/main/java/com/example/pet/my/order/MyOrderActivity.java
@@ -36,6 +36,14 @@ public class MyOrderActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_order);
+        if (Build.VERSION.SDK_INT >= 21) {   //只有5.0及以上系统才支持，因此这里先进行了一层if判断
+            View decorView = getWindow().getDecorView();
+            int option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE    //设置为全屏显示，必须这两行代码一起才能生效
+                    |View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;  //因为背景为浅色，设置通知栏字体颜色为深色
+            decorView.setSystemUiVisibility(option);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);//设置为透明
+        }
         //注册事件订阅者
         EventBus.getDefault().register(this);
 
