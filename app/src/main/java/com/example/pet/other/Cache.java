826e5afedb001daf0feaf40bf554a3050d02fcda
罗润泽 <lrz914@outlook.com>
2020-12-09 12:00:00 +0800

    update

--- app/src/main/java/com/example/pet/other/Cache.java
+++ app/src/main/java/com/example/pet/other/Cache.java
@@ -6,20 +6,20 @@ import com.example.pet.other.entity.Tips;
 import com.example.pet.other.entity.User;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 public class Cache {
     /**
      * 存储一些全局信息
      */
-public static User user;
-public static String userPhone;
-public static final String MY_URL="http://10.7.90.222:8080/ChongBaoService_war_exploded/";//我的URL
-public static String url = "http://10.7.89.96:8080/ChongBao_war_exploded/";
-
-public static int user_id = 1;
-
-public static List<Order> myOrderList ;//我的订单数据
-public static List<Pet> myPetList;//我的宠物数据
-    public static ArrayList<Tips> myPostList ;
+    public static User user;
+    public static String userPhone;
+    public static final String MY_URL = "http://10.7.90.222:8080/ChongBaoService_war_exploded/";//我的URL
+    public static String url = "http://10.7.89.96:8080/ChongBao_war_exploded/";
+    public static int user_id = 1;
+    public static List<Order> myOrderList;//我的订单数据
+    public static List<Pet> myPetList;//我的宠物数据
+    public static ArrayList<Tips> myPostList;//我的帖子数据
+    public static HashSet<User> userHashSet = new HashSet<>();//用户列表
 }
