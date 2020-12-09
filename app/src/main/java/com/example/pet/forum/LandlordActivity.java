8b009b6927824cb172310356a12ecbd7838a2a17
罗润泽 <lrz914@outlook.com>
2020-12-09 11:40:03 +0800

    update

--- app/src/main/java/com/example/pet/forum/LandlordActivity.java
+++ app/src/main/java/com/example/pet/forum/LandlordActivity.java
@@ -1,25 +1,57 @@
 package com.example.pet.forum;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.pet.MainActivity;
 import com.example.pet.R;
+import com.example.pet.other.Cache;
 import com.example.pet.other.entity.Tips;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.concurrent.CountDownLatch;
 
 public class LandlordActivity extends AppCompatActivity {
 
     private ArrayList<Tips> tipsArrayList = new ArrayList<>();
     private ListView listView;
     private LinearLayout btn_back;
+
+    private Handler handler = new Handler(){
+        @Override
+        public void handleMessage(@NonNull Message message) {
+            switch (message.what){
+                case 1:
+                    Log.e("handler", "gethere----------------------");
+                    init((ArrayList) message.obj);
+                    break;
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +68,148 @@ public class LandlordActivity extends AppCompatActivity {
             }
         });
         listView = findViewById(R.id.lv_landlord_tips);
+    }
+
+    public void init(ArrayList arrayList){
+        Log.e("init方法", "----------------------------");
+        getImages(arrayList);
+        getHeadImages(arrayList);
+    }
+
+    public void getImages(final ArrayList<Tips> arrayList) {
+        final CountDownLatch latch = new CountDownLatch(arrayList.size());
+
+        for (int i = 0; i < arrayList.size(); i ++ ) {
+            final Tips tips = arrayList.get(i);
+            new Thread(){
+                @Override
+                public void run() {
+                    try {
+                        String path = tips.getImagepath();
+                        URL url = new URL(Cache.MY_URL + "GetImageByPath?path=" + path);
+                        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                        InputStream in = urlConnection.getInputStream();
+                        Bitmap bitmap = BitmapFactory.decodeStream(in);
+                        tips.setThumbnail(bitmap);
+                        in.close();
+                        latch.countDown();
+                    } catch (MalformedURLException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            }.start();
+        }
+        try {
+            latch.await();
+            initAdapter();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void getHeadImages(final ArrayList<Tips> arrayList) {
+        final CountDownLatch latch = new CountDownLatch(arrayList.size());
+
+        for (int i = 0; i < arrayList.size(); i ++ ) {
+            final Tips tips = arrayList.get(i);
+            new Thread(){
+                @Override
+                public void run() {
+                    try {
+                        String path = tips.getHeadImagepath();
+                        URL url = new URL(Cache.MY_URL + "GetImageByPath?path=" + path);
+                        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                        InputStream in = urlConnection.getInputStream();
+                        Bitmap bitmap = BitmapFactory.decodeStream(in);
+                        tips.setUserHead(bitmap);
+                        in.close();
+                        latch.countDown();
+                    } catch (MalformedURLException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            }.start();
+        }
+        try {
+            latch.await();
+            initAdapter();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void initAdapter() {
+        Log.e("initAdapter方法", "-------------------------");
         LandlordTipsAdapter landlordTipsAdapter = new LandlordTipsAdapter(this, tipsArrayList, R.layout.landlord_tips_item);
         listView.setAdapter(landlordTipsAdapter);
+    }
+
+    public void getAllTips(final int i){
+        new Thread(){
+            @Override
+            public void run() {
+                try {
+                    URL url = new URL(Cache.MY_URL + "GetPostByTime?i="+i);
+                    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                    connection.setRequestMethod("GET");
+                    InputStream input = connection.getInputStream();
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(input));
+                    StringBuffer stringBuffer = new StringBuffer();
+                    String line;
+                    while ((line=bufferedReader.readLine())!=null){
+                        stringBuffer.append(line);
+                    }
+                    tipsArrayList.clear();
+                    JSONArray jsonArray = new JSONArray(stringBuffer.toString());
+                    for (int i=0;i<jsonArray.length();i++){
+                        JSONObject jsonObject = jsonArray.getJSONObject(i);
+                        int post_id = jsonObject.getInt("post_id");
+                        String post_title = jsonObject.getString("post_title");
+                        String post_time = jsonObject.getString("post_time");
+                        String post_text = jsonObject.getString("post_text");
+                        String topic = jsonObject.getString("post_topic");
+                        String user_name = jsonObject.getString("user_name");
+                        int count_likes = jsonObject.getInt("likes");
+                        int count_comments = jsonObject.getInt("comments");
+                        int count_forwards = jsonObject.getInt("forwards");
+                        String img_path = jsonObject.getString("picture_path");
+                        String head_img_path = jsonObject.getString("user_picture_path");
+                        Tips tips = new Tips();
+                        tips.setId(post_id);
+                        tips.setTitle(post_title);
+                        tips.setText(post_text);
+                        tips.setTime(post_time);
+                        tips.setUserName(user_name);
+                        tips.setTopic(topic);
+                        tips.setLikes(count_likes);
+                        tips.setComments(count_comments);
+                        tips.setForwards(count_forwards);
+                        tips.setImagepath(img_path);
+                        tips.setHeadImagepath(head_img_path);
+                        tipsArrayList.add(tips);
+                    }
+                    Message message = handler.obtainMessage();
+                    message.what = 1;
+                    message.obj = tipsArrayList;
+                    handler.sendMessage(message);
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (ProtocolException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
     }
 }
