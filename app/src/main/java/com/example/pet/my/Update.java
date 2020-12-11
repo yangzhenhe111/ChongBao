0e007d3f2296f9ffee69e5dabd2ec84df61933d9
yangzhenhe111 <3258274495@qq.com>
2020-12-11 17:01:39 +0800

    register over

--- app/src/main/java/com/example/pet/my/Update.java
+++ app/src/main/java/com/example/pet/my/Update.java
@@ -5,11 +5,20 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 import com.example.pet.R;
+import com.example.pet.my.editinfo.EditInfo;
 
 public class Update extends AppCompatActivity {
     private Toolbar toolbar;
+    private EditText etPhone;
+    private EditText etYan;
+    private EditText etPassword;
+
+    private String phone;
+    private String password;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +29,25 @@ public class Update extends AppCompatActivity {
 
     private void setView() {
         toolbar = findViewById(R.id.find_toolbar);
+        etPhone = findViewById(R.id.et_phone_find);
+        etYan = findViewById(R.id.et_yanzheng_find);
+        etPassword = findViewById(R.id.et_pass_find);
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Update.this.finish();
             }
         });
+    }
+
+    public void onClicked(View view) {
+        switch (view.getId()){
+            case R.id.btn_getYan_find:
+                //获取验证码
+                break;
+            case R.id.btn_sub_find:
+                //验证验证码，并提交新的密码
+                break;
+        }
     }
 }
